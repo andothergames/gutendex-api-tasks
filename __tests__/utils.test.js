18ae231f5229923942239611d82ce3b3e6845e44
Joy <32204909+andothergames@users.noreply.github.com>
2024-09-12 16:04:50 +0100

    feat: toUppercase utils function and tests

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,14 +1,36 @@
-const { sortByID } = require("../utils/sortByID")
+const { sortByID } = require("../utils/sortByID");
+const { toUppercase } = require("../utils/toUppercase");
 
 describe("sortByID", () => {
-    test("sorts books by ID", () => {
-      const actual = sortByID();
-      const expected = 32;
-      return actual.then((array) => {
-        expect(array.length).toBe(expected);
-        expect(array).toBeSortedBy("id", {
-          ascending: true,
-        });
+  test("sorts books by ID", () => {
+    const actual = sortByID();
+    const expected = 32;
+    return actual.then((array) => {
+      expect(array.length).toBe(expected);
+      expect(array).toBeSortedBy("id", {
+        ascending: true,
       });
     });
+  });
+});
+
+describe("toUppercase", () => {
+  test("returns string as uppercase", () => {
+    const input = "hello";
+    const actual = toUppercase(input);
+    const expected = "HELLO";
+    expect(actual).toBe(expected);
+  });
+  test("returns string as uppercase if mixed with numbers", () => {
+    const input = "thisISaTest100";
+    const actual = toUppercase(input);
+    const expected = "THISISATEST100";
+    expect(actual).toBe(expected);
+  });
+  test("returns string as uppercase if mixed with symbols", () => {
+    const input = "this -- is a / test";
+    const actual = toUppercase(input);
+    const expected = "THIS -- IS A / TEST";
+    expect(actual).toBe(expected);
+  });
 });
